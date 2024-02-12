55ad92e51fba1df40fb73573f57a8c963ffa0bf1
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-02-12 14:54:02 +0530

    No new Changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex gap-4">
       <h1>This is an protected route</h1>
       <UserButton />
     </div>
