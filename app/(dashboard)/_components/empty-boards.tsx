30f2d35f8f28bcd461f3b473de3affe6260b7b65
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-03-13 22:54:42 +0530

    Creating create API for boards

---
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -0,0 +1,31 @@
+"use client";
+import {useMutation} from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { useOrganization } from "@clerk/nextjs";
+
+export const EmptyBoards=()=>{
+    const {organization}=useOrganization();
+    const create=useMutation(api.board.create);
+    const onClick=()=>{
+        if(!organization) return;
+        create({
+            orgId:organization.id,
+            title:"untitled"
+        })
+    }
+    return(
+        <div className='h-full flex flex-col items-center justify-center'>
+            <Image src="/note.svg" height={110} width={110} alt='empty' />
+            <h2 className='text-2xl font-semibold mt-6'>Create your first board</h2>
+            <p className='text-muted-foreground text-sm mt-2'>Start by creating a board for your organization</p>
+            <div className='mt-6'>
+                <Button onClick={onClick} size='lg'>
+                    Create board
+                </Button>
+            </div>
+        </div>
+    )
+}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,26 @@
-const DashboardPage=()=>{
+"use client";
+
+import { BoardList } from "./_components/boards-list";
+import { EmptyOrg } from "./_components/empty-org";
+import { useOrganization } from "@clerk/nextjs";
+
+
+interface DashboardPageProps{
+    searchParams:{
+        search?:string,
+        favorites?:string
+    }
+}
+
+const DashboardPage=({searchParams}:DashboardPageProps)=>{
+    const {organization}=useOrganization();
     return(
-        <div>
-            <h1>Dashboard Page</h1>
+        <div className="flex-1 h-[calc(100%-80px)] p-6">
+            {!organization ? (
+                <EmptyOrg />
+            ):(
+                <BoardList orgId={organization.id} query={searchParams}/>
+            )}
         </div>
     )
 }
