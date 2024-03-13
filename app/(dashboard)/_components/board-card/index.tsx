b2777a810e65001b0c0d993ef33dd2d58dcdea64
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-03-14 00:07:22 +0530

    Added Board Page

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import {formatDistanceToNow} from 'date-fns';
+import Image from "next/image";
+import { Overlay } from "./overlay";
+import { useAuth } from "@clerk/nextjs";
+interface BoardCardProps{
+    id:string,
+    title:string,
+    authorName:string,
+    authorId:string,
+    createdAt:number,
+    imageUrl:string,
+    orgId:string,
+    isFavorite:boolean
+}
+
+export const BoardCard=(
+    {id,title,authorId,authorName,createdAt,imageUrl,orgId,isFavorite}:BoardCardProps
+)=>{
+    const {userId}=useAuth();
+    const authorLabel=userId===authorId ? "You" : authorName;
+    const createdAtLabel=formatDistanceToNow(createdAt,{
+        addSuffix:true
+    })
+
+
+    
+    return(
+        <Link href={`/board/${id}`}>
+            <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+                <div className="relative flex-1 bg-amber-50">
+                    <Image src={imageUrl} alt={title} fill className="object-fit" />
+                    <Overlay />
+                </div>
+            </div>
+        </Link>
+    )
+}
--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,19 +1,27 @@
 "use client";
-import {useMutation} from "convex/react";
+
 import { api } from "@/convex/_generated/api";
 
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
 
 export const EmptyBoards=()=>{
     const {organization}=useOrganization();
-    const create=useMutation(api.board.create);
+    const {mutate,pending}=useApiMutation(api.board.create);
     const onClick=()=>{
         if(!organization) return;
-        create({
+        mutate({
             orgId:organization.id,
             title:"untitled"
+        })
+        .then((id)=>{
+            toast.success("Board created");
+        })
+        .catch(()=>{
+            toast.error("Failed to create board")
         })
     }
     return(
@@ -22,7 +30,7 @@ export const EmptyBoards=()=>{
             <h2 className='text-2xl font-semibold mt-6'>Create your first board</h2>
             <p className='text-muted-foreground text-sm mt-2'>Start by creating a board for your organization</p>
             <div className='mt-6'>
-                <Button onClick={onClick} size='lg'>
+                <Button disabled={pending} onClick={onClick} size='lg'>
                     Create board
                 </Button>
             </div>
