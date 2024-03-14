de8cf17a282888e351ddd07cd4fe3972b67506fe
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-03-14 23:44:49 +0530

    Completed Delete Functionality

--- components/actions.tsx
+++ components/actions.tsx
@@ -3,8 +3,10 @@
 import { DropdownMenuContentProps, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 
 import {DropdownMenu,DropdownMenuSubTrigger,DropdownMenuContent,DropdownMenuItem,DropdownMenuSeparator} from "@/components/ui/dropdown-menu"
-import { Link2 } from "lucide-react";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
 
 interface ActionProps{
     children:React.ReactNode
@@ -16,10 +18,18 @@ interface ActionProps{
 
 export const Actions=({children,side,sideOffset,id,title}:ActionProps)=>{
 
+    const {mutate,pending}=useApiMutation(api.board.remove);
+
     const onCopyLink=()=>{
         navigator.clipboard.writeText(`${window.location.origin}/board/${id}`)
         .then(()=>toast.success("Link copied"))
         .catch(()=>toast.error("Failed to copy link"))
+    }
+
+    const onDelete=()=>{
+        mutate({id})
+        .then(()=>toast.success("Board deleted"))
+        .catch(()=>toast.error("Failed to delete board"))
     }
 
     return(
@@ -31,6 +41,10 @@ export const Actions=({children,side,sideOffset,id,title}:ActionProps)=>{
                 <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
                     <Link2 className="h-4 w-4 mr-2" />
                     Copy board link 
+                </DropdownMenuItem>
+                <DropdownMenuItem className="p-3 cursor-pointer" onClick={onDelete}>
+                    <Trash2 className="h-4 w-4 mr-2" />
+                    Delete
                 </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
